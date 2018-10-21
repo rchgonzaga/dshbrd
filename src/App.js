e0747b1bc569eae08be2a5b1372bf79516c74c57
rchgonzaga@gmail.com <rchgonzaga@gmail.com>
2018-10-21 10:08:16 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import 'semantic-ui-css/semantic.min.css';
+import { Button, Header, Icon, Image, Menu, Container, Sidebar } from 'semantic-ui-react'
 
 class App extends Component {
+    state = { visible: false }
+
+  handleButtonClick = () => this.setState({ visible: !this.state.visible })
+
+  handleSidebarHide = () => this.setState({ visible: false })
+
   render() {
+    const { visible } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div>
+        <Button onClick={this.handleButtonClick}>Toggle visibility</Button>
+
+        <Sidebar.Pushable>
+          <Sidebar
+            as={Menu}
+            animation='overlay'
+            icon='labeled'
+            inverted
+            onHide={this.handleSidebarHide}
+            vertical
+            visible={visible}
+            width='thin'
           >
-            Learn React
-          </a>
-        </header>
+            <Menu.Item as='a'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='gamepad' />
+              Games
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='camera' />
+              Channels
+            </Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher>
+            <Container fluid style={{height: window.outerHeight}}>
+              <Header as='h3'>Application Content</Header>
+              <Image src='/images/wireframe/paragraph.png' />
+            </Container>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
       </div>
     );
   }
