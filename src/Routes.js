f5df5dbd3cd04da9403277a069bbb84f577bb6df
rchgonzaga@gmail.com <rchgonzaga@gmail.com>
2018-10-23 00:55:38 -0300

    Fixing selected menu item

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import React from "react"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 
 import {
   Header,
@@ -8,21 +8,22 @@ import {
   Container,
   Sidebar,
   Dropdown
-} from 'semantic-ui-react'
-import { Subscribe } from 'unstated'
+} from "semantic-ui-react"
+import { Subscribe } from "unstated"
 
-import ApiSubscribe from './state/Api'
+import ApiSubscribe from "./state/Api"
 
-import Home from './pages/Home'
-import Foo from './pages/Foo'
-import Bar from './pages/Bar'
-import Counter from './components/Counter'
+import Home from "./pages/Home"
+import Foo from "./pages/Foo"
+import Bar from "./pages/Bar"
+import Counter from "./components/Counter"
 
 class Routes extends React.Component {
   state = {
     visible: false,
     width: 800,
-    height: 182
+    height: 182,
+    activeItem: ''
   }
 
   /**
@@ -30,14 +31,14 @@ class Routes extends React.Component {
    */
   componentDidMount() {
     this.updateDimensions()
-    window.addEventListener('resize', this.updateDimensions.bind(this))
+    window.addEventListener("resize", this.updateDimensions.bind(this))
   }
 
   /**
    * Remove event listener
    */
   componentWillUnmount() {
-    window.removeEventListener('resize', this.updateDimensions.bind(this))
+    window.removeEventListener("resize", this.updateDimensions.bind(this))
   }
 
   /**
@@ -53,7 +54,10 @@ class Routes extends React.Component {
     // }
   }
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  handleItemClick = (e, { name }) => {
+    console.log(name);
+    this.setState({ activeItem: name })
+  }
 
   // handleButtonClick = () => this.setState({ visible: !this.state.visible })
 
@@ -64,151 +68,167 @@ class Routes extends React.Component {
     return (
       <Subscribe to={[ApiSubscribe]}>
         {appContainer => (
-          <div>
-            <Menu attached='top' size='large' inverted style={{backgroundColor: '#003f62'}}>
-
-              {/* Shows only if logged = true */}
-              {appContainer.state.loggedIn ? (
-                <Menu.Item
-                  icon='bars'
-                  simple='true'
-                  onClick={() => appContainer.handleSideMenu()}
-                />
-              ) : (
-                ''
-              )}
-
-              {/* Shows only if logged = true */}
-              {appContainer.state.loggedIn ? (
-                <Menu.Item
-                  name='item1'
-                  active={activeItem === 'item1'}
-                  onClick={this.handleItemClick}
-                  style={{ display: appContainer.state.loggedIn ? '' : 'none' }}
-                >
-                  Item 1
-                </Menu.Item>
-              ) : (
-                ''
-              )}
-
-              {/* Shows only if logged = true */}
-              {appContainer.state.loggedIn ? (
-                <Menu.Item
-                  name='item2'
-                  active={activeItem === 'item2'}
-                  onClick={this.handleItemClick}
-                  style={{ display: appContainer.state.loggedIn ? '' : 'none' }}
-                >
-                  Item 2
-                </Menu.Item>
-              ) : (
-                ''
-              )}
-
-              {/* Shows only if logged = true */}
-              {appContainer.state.loggedIn ? (
-                <Dropdown
-                  item
-                  icon='wrench'
-                  simple
-                  style={{ display: appContainer.state.loggedIn ? '' : 'none' }}
-                >
-                  <Dropdown.Menu>
-                    <Dropdown.Item>
-                      <Icon name='dropdown' />
-                      <span className='text'>New</span>
-
-                      <Dropdown.Menu>
-                        <Dropdown.Item>Document</Dropdown.Item>
-                        <Dropdown.Item>Image</Dropdown.Item>
-                      </Dropdown.Menu>
-                    </Dropdown.Item>
-                    <Dropdown.Item>Open</Dropdown.Item>
-                    <Dropdown.Item>Save...</Dropdown.Item>
-                    <Dropdown.Item>Edit Permissions</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Header>Export</Dropdown.Header>
-                    <Dropdown.Item>Share</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              ) : (
-                ''
-              )}
-
-              <Menu.Menu position='right'>
-                <div className='ui right aligned category search item'>
-                  <div className='ui transparent icon input'>
-                    <Counter />
-                    <input
-                      className='prompt'
-                      type='text'
-                      placeholder='Search animals...'
-                    />
-                    <i className='search link icon' />
-                  </div>
-                  <div className='results' />
-                </div>
-              </Menu.Menu>
-            </Menu>
-
-            <Sidebar.Pushable>
-
-              {/* Shows only if logged = true */}
-              {appContainer.state.loggedIn ? (
-              <Sidebar
-                as={Menu}
-                animation='push'
-                icon='labeled'
+          <Router>
+            <div>
+              <Menu
+                attached="top"
+                size="large"
                 inverted
-                onHide={this.handleSidebarHide}
-                vertical
-                visible={appContainer.state.sideMenuVisible}
-                width='thin'
-                style={{ display: appContainer.state.loggedIn ? '' : 'none', backgroundColor: '#055885' }}
+                style={{ backgroundColor: "#003f62" }}
               >
-                <Menu.Item as='a'>
-                  <Icon name='home' />
-                  <Counter />
-                </Menu.Item>
-                <Menu.Item as='a'>
-                  <Icon name='gamepad' />
-                  Games
-                </Menu.Item>
-                <Menu.Item as='a'>
-                  <Icon name='camera' />
-                  Channels
-                  <Counter />
-                </Menu.Item>
-              </Sidebar>
-              ) : (
-                ''
-              )}
+                {/* Shows only if logged = true */}
+                {appContainer.state.loggedIn ? (
+                  <Menu.Item
+                    icon="bars"
+                    simple="true"
+                    onClick={() => appContainer.handleSideMenu()}
+                  />
+                ) : (
+                  ""
+                )}
 
-              <Sidebar.Pusher>
-                <Container
-                  fluid
-                  style={{ height: this.state.height, padding: '10px' }}
-                >
-                  <div>
-                    <Header as='h3'>Application Content</Header>
-                    <span>{appContainer.state.count}</span>
-                    <Router>
-                      <div>
-                        <Link to='/'>Home</Link> |&nbsp;
-                        <Link to='/foo'>Foo</Link> |&nbsp;
-                        <Link to='/bar'>Bar</Link>
-                        <hr />
-                        <Route exact path='/' component={Home} />
-                        <Route path='/foo' component={Foo} />
-                        <Route path='/bar' component={Bar} />
-                      </div>
-                    </Router>
+                {/* Shows only if logged = true */}
+                {appContainer.state.loggedIn ? (
+                  <Menu.Item
+                    active={activeItem === "home"}
+                    onClick={this.handleItemClick}
+                    style={{
+                      display: appContainer.state.loggedIn ? "" : "none"
+                    }}
+                    as={Link}
+                    name="home"
+                    to="/"
+                  >
+                    Home
+                  </Menu.Item>
+                ) : (
+                  ""
+                )}
+
+                {/* Shows only if logged = true */}
+                {appContainer.state.loggedIn ? (
+                  <Menu.Item
+                    active={activeItem === "foo"}
+                    onClick={this.handleItemClick}
+                    style={{
+                      display: appContainer.state.loggedIn ? "" : "none"
+                    }}
+                    as={Link}
+                    name="foo"
+                    to="/foo"
+                  >
+                    Foo
+                  </Menu.Item>
+                ) : (
+                  ""
+                )}
+
+                {/* Shows only if logged = true */}
+                {appContainer.state.loggedIn ? (
+                  <Dropdown
+                    item
+                    icon="wrench"
+                    simple
+                    style={{
+                      display: appContainer.state.loggedIn ? "" : "none"
+                    }}
+                  >
+                    <Dropdown.Menu>
+                      <Dropdown.Item>
+                        <Icon name="dropdown" />
+                        <span className="text">New</span>
+
+                        <Dropdown.Menu>
+                          <Dropdown.Item>Document</Dropdown.Item>
+                          <Dropdown.Item>Image</Dropdown.Item>
+                        </Dropdown.Menu>
+                      </Dropdown.Item>
+                      <Dropdown.Item>Open</Dropdown.Item>
+                      <Dropdown.Item>Save...</Dropdown.Item>
+                      <Dropdown.Item>Edit Permissions</Dropdown.Item>
+                      <Dropdown.Divider />
+                      <Dropdown.Header>Export</Dropdown.Header>
+                      <Dropdown.Item>Share</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                ) : (
+                  ""
+                )}
+
+                <Menu.Menu position="right">
+                  <div className="ui right aligned category search item">
+                    <div className="ui transparent icon input">
+                      <Counter />
+                      <input
+                        className="prompt"
+                        type="text"
+                        placeholder="Search animals..."
+                      />
+                      <i className="search link icon" />
+                    </div>
+                    <div className="results" />
                   </div>
-                </Container>
-              </Sidebar.Pusher>
-            </Sidebar.Pushable>
-          </div>
+                </Menu.Menu>
+              </Menu>
+
+              <Sidebar.Pushable>
+                {/* Shows only if logged = true */}
+                {appContainer.state.loggedIn ? (
+                  <Sidebar
+                    as={Menu}
+                    animation="push"
+                    icon="labeled"
+                    inverted
+                    onHide={this.handleSidebarHide}
+                    vertical
+                    visible={appContainer.state.sideMenuVisible}
+                    width="thin"
+                    style={{
+                      display: appContainer.state.loggedIn ? "" : "none",
+                      backgroundColor: "#055885"
+                    }}
+                  >
+                    <Menu.Item as="a">
+                      <Icon name="home" />
+                      <Counter />
+                    </Menu.Item>
+                    <Menu.Item as="a">
+                      <Icon name="gamepad" />
+                      Games
+                    </Menu.Item>
+                    <Menu.Item as="a">
+                      <Icon name="camera" />
+                      Channels
+                      <Counter />
+                    </Menu.Item>
+                  </Sidebar>
+                ) : (
+                  ""
+                )}
+
+                <Sidebar.Pusher>
+                  <Container
+                    fluid
+                    style={{ height: this.state.height, padding: "10px" }}
+                  >
+                    <div>
+                      <Header as="h3">Application Content</Header>
+                      <span>{appContainer.state.count}</span>
+                      <div>
+                        <Link to="/">Home</Link> |&nbsp;
+                        <Link to="/foo">Foo</Link> |&nbsp;
+                        <Link to="/bar">Bar</Link>
+                        <hr />
+                        <Route exact path="/" component={Home} />
+                        <Route path="/foo" component={Foo} />
+                        <Route path="/bar" component={Bar} />
+                      </div>
+                    </div>
+                  </Container>
+                </Sidebar.Pusher>
+              </Sidebar.Pushable>
+            </div>
+          </Router>
         )}
       </Subscribe>
     )
