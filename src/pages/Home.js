e9531206c5e3ec260adea3ef597619e6af6da2aa
GONZAGA <EVSKW@monsanto.com>
2018-10-25 10:21:16 -0300

    Adding statistics

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,165 +9,142 @@ import { PieChart } from "./dash/components/PieChart"
 import Hr from "../components/Hr"
 import LoaderSpinner from "../components/Loader"
 import { HorizontalGroupedBars } from "./dash/components/HorizontalGroupedBars"
+import StatisticItems from "./dash/components/StatisticItems";
 
 class HomeChild extends React.Component {
-  constructor() {
-    super()
-  }
 
-  componentDidMount() {
-    this.props.api.getCurrentSession()
-  }
+    componentDidMount() {
+        this.props.api.getCurrentSession()
+    }
 
-  render() {
-    let { api } = this.props
-    return (
-      <div>
-        {/* NORMAL STATE */}
-        {api.state.isLoadingSession == false ? (
-          <div>
-            <h1>
-              🏠 Home
-              <Button primary onClick={() => api.changeAiMothaFocka()}>Update</Button>
-              <Button secondary onClick={() => api.getCurrentSession()}>Update Grid</Button>
-            </h1>
-            <Hr />
-            <Grid columns={2}>
-              <Grid.Row>
-                <Grid.Column>
-                  <PieChart
-                    data={api.state.barData}
-                    width={window.innerWidth / 2}
-                    height={window.innerHeight / 2}
-                  />
-                </Grid.Column>
-                <Grid.Column>
-                  <HorizontalGroupedBars
-                    data={api.state.pieData}
-                    width={window.innerWidth / 2}
-                    height={window.innerHeight / 2}
-                  />
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-            <Hr />
+    render() {
+        let { api } = this.props
+        return (
+            <div>
+                {/* NORMAL STATE */}
+                {api.state.isLoadingSession === false ? (
+                    <div>
+                        <h1>
+                            🏠 Home
+                            <Button primary onClick={() => api.changeAiMothaFocka()}>Update</Button>
+                            <Button secondary onClick={() => api.getCurrentSession()}>Update Grid</Button>
+                        </h1>
+                        <Hr />
+                        <Grid columns={2}>
+                            <Grid.Row>
+                                <Grid.Column>
+                                    <PieChart
+                                        data={api.state.barData}
+                                        width={window.innerWidth / 2}
+                                        height={window.innerHeight / 2}
+                                    />
+                                </Grid.Column>
+                                <Grid.Column>
+                                    <HorizontalGroupedBars
+                                        data={api.state.pieData}
+                                        width={window.innerWidth / 2}
+                                        height={window.innerHeight / 2}
+                                    />
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                        <Hr />
 
-            <Statistic.Group widths="four">
-              <Statistic>
-                <Statistic.Value>22</Statistic.Value>
-                <Statistic.Label>Saves</Statistic.Label>
-              </Statistic>
+                        <StatisticItems data={{
+                            originalActivities: [],
+                            totalSubjectClosed: 31,
+                            totalSubjectOpen: 12,
+                            totalSubjectWaitingUser: 78,
+                            totalL2Open: 2,
+                            totalL3LegacyOpen: 5,
+                            totalL3CloudOpen: 7
+                        }}/>
 
-              <Statistic>
-                <Statistic.Value text>
-                  Three
-                  <br />
-                  Thousand
-                </Statistic.Value>
-                <Statistic.Label>Signups</Statistic.Label>
-              </Statistic>
-
-              <Statistic>
-                <Statistic.Value>
-                  <Icon name="hourglass" />5
-                </Statistic.Value>
-                <Statistic.Label>Flights</Statistic.Label>
-              </Statistic>
-
-              <Statistic>
-                <Statistic.Value>
-                  <Icon name="cloud" />
-                  42
-                </Statistic.Value>
-                <Statistic.Label>Team Members</Statistic.Label>
-              </Statistic>
-            </Statistic.Group>
-
-            <Hr />
-            <ReactTable
-              data={api.state.ticketList}
-              filterable
-              defaultFilterMethod={(filter, row) =>
-                String(row[filter.id]) === filter.value
-              }
-              columns={[
-                {
-                  Header: "Name",
-                  columns: [
-                    {
-                      Header: "First Name",
-                      accessor: "name",
-                      filterMethod: (filter, row) =>
-                        row[filter.id].startsWith(filter.value) &&
-                        row[filter.id].endsWith(filter.value)
-                    },
-                    {
-                      Header: "Last Name",
-                      id: "username",
-                      accessor: d => d.username,
-                      filterMethod: (filter, rows) =>
-                        matchSorter(rows, filter.value, {
-                          keys: ["username"]
-                        }),
-                      filterAll: true
-                    }
-                  ]
-                },
-                {
-                  Header: "Info",
-                  columns: [
-                    {
-                      Header: "Age",
-                      accessor: "id"
-                    },
-                    {
-                      Header: "Over 3",
-                      accessor: "id",
-                      id: "over",
-                      Cell: ({ value }) => (value >= 3 ? "Yes" : "No"),
-                      filterMethod: (filter, row) => {
-                        if (filter.value === "all") {
-                          return true
-                        }
-                        if (filter.value === "true") {
-                          return row[filter.id] >= 3
-                        }
-                        return row[filter.id] < 3
-                      },
-                      Filter: ({ filter, onChange }) => (
-                        <select
-                          onChange={event => onChange(event.target.value)}
-                          style={{ width: "100%" }}
-                          value={filter ? filter.value : "all"}
-                        >
-                          <option value="all">Show All</option>
-                          <option value="true">Can Drink</option>
-                          <option value="false">Can't Drink</option>
-                        </select>
-                      )
-                    }
-                  ]
-                }
-              ]}
-              defaultPageSize={10}
-              className="-striped -highlight"
-            />
-            <Grid columns={2}>
-              <Grid.Row>
-                <Grid.Column>as</Grid.Column>
-                <Grid.Column>adasdsa</Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </div>
-        ) : (
-          <div>
-            {/* LOADING STATE */}
-            <LoaderSpinner />
-          </div>
-        )}
-      </div>
-    )
-  }
+                        <Hr />
+                        <ReactTable
+                            data={api.state.ticketList}
+                            filterable
+                            defaultFilterMethod={(filter, row) =>
+                                String(row[filter.id]) === filter.value
+                            }
+                            columns={[
+                                {
+                                    Header: "Name",
+                                    columns: [
+                                        {
+                                            Header: "First Name",
+                                            accessor: "name",
+                                            filterMethod: (filter, row) =>
+                                                row[filter.id].startsWith(filter.value) &&
+                                                row[filter.id].endsWith(filter.value)
+                                        },
+                                        {
+                                            Header: "Last Name",
+                                            id: "username",
+                                            accessor: d => d.username,
+                                            filterMethod: (filter, rows) =>
+                                                matchSorter(rows, filter.value, {
+                                                    keys: ["username"]
+                                                }),
+                                            filterAll: true
+                                        }
+                                    ]
+                                },
+                                {
+                                    Header: "Info",
+                                    columns: [
+                                        {
+                                            Header: "Age",
+                                            accessor: "id"
+                                        },
+                                        {
+                                            Header: "Over 3",
+                                            accessor: "id",
+                                            id: "over",
+                                            Cell: ({ value }) => (value >= 3 ? "Yes" : "No"),
+                                            filterMethod: (filter, row) => {
+                                                if (filter.value === "all") {
+                                                    return true
+                                                }
+                                                if (filter.value === "true") {
+                                                    return row[filter.id] >= 3
+                                                }
+                                                return row[filter.id] < 3
+                                            },
+                                            Filter: ({ filter, onChange }) => (
+                                                <select
+                                                    onChange={event => onChange(event.target.value)}
+                                                    style={{ width: "100%" }}
+                                                    value={filter ? filter.value : "all"}
+                                                >
+                                                    <option value="all">Show All</option>
+                                                    <option value="true">Can Drink</option>
+                                                    <option value="false">Can't Drink</option>
+                                                </select>
+                                            )
+                                        }
+                                    ]
+                                }
+                            ]}
+                            defaultPageSize={10}
+                            className="-striped -highlight"
+                        />
+                        <Grid columns={2}>
+                            <Grid.Row>
+                                <Grid.Column>as</Grid.Column>
+                                <Grid.Column>adasdsa</Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </div>
+                ) : (
+                        <div>
+                            {/* LOADING STATE */}
+                            <LoaderSpinner />
+                        </div>
+                    )}
+            </div>
+        )
+    }
 }
 
 // const Home = () => {
---
+++ src/pages/dash/components/StatisticItems.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Statistic, Icon } from "semantic-ui-react"
+
+const StatisticItems = (props) => {
+    
+    return (
+        <Statistic.Group widths='eight'>
+
+            <Statistic color='grey'>
+                <Statistic.Value><Icon name='comments' /> {props.data.originalActivities.length}</Statistic.Value>
+                <Statistic.Label>Subjects</Statistic.Label>
+            </Statistic>
+
+            <Statistic>
+                <Statistic.Value style={{ color: '#c1c1c1' }}><Icon name='lock' /> {props.data.totalSubjectClosed}</Statistic.Value>
+                <Statistic.Label>Closed</Statistic.Label>
+            </Statistic>
+
+            <Statistic>
+                <Statistic.Value style={{ color: '#b2e8ab' }}><Icon name='lock open' /> {props.data.totalSubjectOpen + props.data.totalSubjectWaitingUser}</Statistic.Value>
+                <Statistic.Label>Open</Statistic.Label>
+            </Statistic>
+
+            <Statistic color='grey'>
+                <Statistic.Value>|</Statistic.Value>
+                <Statistic.Label>&nbsp;</Statistic.Label>
+            </Statistic>
+
+            <Statistic>
+                <Statistic.Value style={{ color: '#e8abab' }} ><Icon name='hourglass one' /> {props.data.totalSubjectWaitingUser}</Statistic.Value>
+                <Statistic.Label>L2 pending</Statistic.Label>
+            </Statistic>
+
+            <Statistic>
+                <Statistic.Value style={{ color: '#b2e8ab' }} ><Icon name='pin' /> {props.data.totalL2Open}</Statistic.Value>
+                <Statistic.Label>L2 Open</Statistic.Label>
+            </Statistic>
+
+            <Statistic color='black'>
+                <Statistic.Value >
+                    <Icon name='chess rock' />{props.data.totalL3LegacyOpen}</Statistic.Value>
+                <Statistic.Label>L3 Legacy</Statistic.Label>
+            </Statistic>
+
+            <Statistic color='blue'>
+                <Statistic.Value >
+                    <Icon name='cloud' />{props.data.totalL3CloudOpen}</Statistic.Value>
+                <Statistic.Label>L3 Cloud</Statistic.Label>
+            </Statistic>
+
+        </Statistic.Group>
+    )
+}
+
+export default StatisticItems
