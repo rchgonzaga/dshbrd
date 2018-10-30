aa85d10f18d896f992d64ed4a58a576374a6adc3
rchgonzaga@gmail.com <rchgonzaga@gmail.com>
2018-10-30 19:28:02 -0300

    Little fix in the Routes and Dash layout

--- src/pages/dash/DashCloudLegacy.js
+++ src/pages/dash/DashCloudLegacy.js
@@ -7,93 +7,92 @@ import { PieChart } from "../dash/components/PieChart"
 import Hr from "../../components/Hr"
 import LoaderSpinner from "../../components/Loader"
 import { HorizontalGroupedBars } from "../dash/components/HorizontalGroupedBars"
-import StatisticItems from "../dash/components/StatisticItems";
-import MainGrid from "../dash/components/MainGrid";
-import HomeApi from "../../state/HomeState";
+import StatisticItems from "../dash/components/StatisticItems"
+import MainGrid from "../dash/components/MainGrid"
+import HomeApi from "../../state/HomeState"
 
 class HomeChild extends React.Component {
+  componentDidMount() {
+    this.props.api.getCurrentSession()
+  }
 
-    componentDidMount() {
-        this.props.api.getCurrentSession()
-    }
+  render() {
+    let { api, globalApi } = this.props
 
-    render() {
-        let { api, globalApi } = this.props
+    return (
+      <div>
+        {/* NORMAL STATE */}
+        {api.state.isLoadingSession === false ? (
+          <div>
+            <h2>
+              Dashboard -{" "}
+              <span style={{ color: "grey" }}>
+                01/06/2018 ~ {new Date().toLocaleDateString("pt-BR")}
+              </span>
+              &nbsp;&nbsp; | &nbsp;&nbsp; Last update:{" "}
+              <span style={{ color: "grey" }}>
+                {" "}
+                {new Date().toLocaleDateString("pt-BR")} &nbsp;&nbsp;
+                <Button primary onClick={() => globalApi.changeAiMothaFocka()}>
+                  Update
+                </Button>
+                <Button secondary onClick={() => api.getCurrentSession()}>
+                  Update Grid
+                </Button>
+              </span>
+            </h2>
+            <Hr />
+            <Grid columns={2}>
+              <Grid.Row>
+                <Grid.Column>
+                  <PieChart
+                    data={api.state.barData}
+                    width={window.innerWidth / 2}
+                    height={window.innerHeight / 2}
+                  />
+                </Grid.Column>
+                <Grid.Column>
+                  <HorizontalGroupedBars
+                    data={api.state.pieData}
+                    width={window.innerWidth / 2}
+                    height={window.innerHeight / 2}
+                  />
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+            <Hr />
 
-        return (
-            <div>
-                {/* NORMAL STATE */}
-                {api.state.isLoadingSession === false ? (
-                    <div>
-                        <h2>
-                            Dashboard - <span style={{ color: 'grey' }}>01/06/2018 ~ {(new Date()).toLocaleDateString("pt-BR")}</span>
-                            &nbsp;&nbsp; | &nbsp;&nbsp; Last update: <span style={{ color: 'grey' }}> {(new Date()).toLocaleDateString("pt-BR")} &nbsp;&nbsp;
-                            <Button primary onClick={() => globalApi.changeAiMothaFocka()}>Update</Button>
-                                <Button secondary onClick={() => api.getCurrentSession()}>Update Grid</Button>
-                            </span>
-                        </h2>
-                        <Hr />
-                        <Grid columns={2}>
-                            <Grid.Row>
-                                <Grid.Column>
-                                    <PieChart
-                                        data={api.state.barData}
-                                        width={window.innerWidth / 2}
-                                        height={window.innerHeight / 2}
-                                    />
-                                </Grid.Column>
-                                <Grid.Column>
-                                    <HorizontalGroupedBars
-                                        data={api.state.pieData}
-                                        width={window.innerWidth / 2}
-                                        height={window.innerHeight / 2}
-                                    />
-                                </Grid.Column>
-                            </Grid.Row>
-                        </Grid>
-                        <Hr />
+            <StatisticItems
+              data={{
+                originalActivities: [],
+                totalSubjectClosed: 31,
+                totalSubjectOpen: 12,
+                totalSubjectWaitingUser: 78,
+                totalL2Open: 2,
+                totalL3LegacyOpen: 5,
+                totalL3CloudOpen: 7
+              }}
+            />
 
-                        <StatisticItems data={{
-                            originalActivities: [],
-                            totalSubjectClosed: 31,
-                            totalSubjectOpen: 12,
-                            totalSubjectWaitingUser: 78,
-                            totalL2Open: 2,
-                            totalL3LegacyOpen: 5,
-                            totalL3CloudOpen: 7
-                        }} />
-
-                        <Hr />
-                        <MainGrid data={api.state.ticketList} />
-                        <Grid columns={2}>
-                            <Grid.Row>
-                                <Grid.Column>as</Grid.Column>
-                                <Grid.Column>adasdsa</Grid.Column>
-                            </Grid.Row>
-                        </Grid>
-                    </div>
-                ) : (
-                        <div>
-                            {/* LOADING STATE */}
-                            <LoaderSpinner />
-                        </div>
-                    )}
-            </div>
-        )
-    }
+            <Hr />
+            <MainGrid data={api.state.ticketList} />
+            <br />
+          </div>
+        ) : (
+          <div>
+            {/* LOADING STATE */}
+            <LoaderSpinner />
+          </div>
+        )}
+      </div>
+    )
+  }
 }
 
-// const Home = () => {
-//   return (
-//     <ApiSubscribe to={[Api]}>
-//       {(api) => (
-//         <HomeChild api={api} />
-//       )}
-//     </ApiSubscribe>
-//     )
-// }
-const Home = () => <ApiSubscribe to={[Api, HomeApi]}>{
-    (api, homeApi) => <HomeChild api={homeApi} globalApi={api}/>
-}</ApiSubscribe>
+const Home = () => (
+  <ApiSubscribe to={[Api, HomeApi]}>
+    {(api, homeApi) => <HomeChild api={homeApi} globalApi={api} />}
+  </ApiSubscribe>
+)
 
 export default Home
