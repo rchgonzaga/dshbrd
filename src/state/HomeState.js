a3ba888c59a20f8a615b4ec2f01e29f5babf409e
GONZAGA <EVSKW@monsanto.com>
2018-11-13 00:19:27 -0200

    Adding extractSLA method to calculate time between tickets

--- src/state/HomeState.js
+++ src/state/HomeState.js
@@ -29,7 +29,57 @@ export class HomeContainer extends Container {
     }
 
     changeAiMothaFocka() {
+        this.extractSLAS()
     }
+
+    extractSLAS() {
+        let count = 0
+
+        let data = this.state.ticketList
+
+        let obj = data
+        let line = '\ufeffSUBJECT;L2 START - INC/WO;CREATED_AT;L2 - WO;CREATED_AT;CLOSED_AT;L3 START - JIRA; CREATED_AT; ASSINED_AD; CLOSED_AT;\n'
+        obj.map((item, index) => {
+
+            count++
+
+            line += item.pai_product_model_version + ';'
+
+            if (item.relations.length > 0) {
+                let min = _.minBy(item.relations, function (o) { return o.filho_submit_date; })
+                if (min) {
+                    // console.log('INICIO L2: ' + min.filho_ticket_number + ' - submit_date: ', min.filho_submit_date)
+                    line += (min.filho_ticket_number ? min.filho_ticket_number : 'N/A') + ';' + (min.filho_submit_date ? min.filho_submit_date : 'N/A') + ';'
+                    
+                    // console.log('INICIO L2: ' + item.ticket_number + ' - submit_date: ', min.pai_submit_date, ' - pai_date_closed: ', min.pai_closed_date)
+                    line += (item.ticket_number ? item.ticket_number : 'N/A') + ';' + (min.pai_submit_date ? min.pai_submit_date : 'N/A') + ';' + (min.pai_closed_date ? min.pai_closed_date : 'N/A') + ';'
+                } else {
+                    // console.log('INICIO L2: ' + item.ticket_number + ' - submit_date: ', item.pai_submit_date, ' - pai_date_closed: ', item.pai_closed_date)
+                    line += ';;' + (item.ticket_number ? item.ticket_number : 'N/A') + ';' + (item.pai_submit_date ? item.pai_submit_date : 'N/A') + ';' + (item.pai_closed_date ? item.pai_closed_date : 'N/A') + ';'
+                }
+            } else {
+                // console.log('INICIO L2: ' + item.ticket_number + ' - submit_date: ', item.pai_submit_date, ' - pai_last_modified_date: ', item.pai_last_modified_date, ' - filho_updated_date: ', item.filho_updated_date)
+                line += ';;' + (item.ticket_number ? item.ticket_number : 'N/A') + ';' + (item.pai_submit_date ? item.pai_submit_date : 'N/A') + ';' + (item.pai_last_modified_date ? item.pai_last_modified_date : 'N/A') + ';' + (item.filho_updated_date ? item.filho_updated_date : 'N/A') + ';'
+            }
+
+            item.relations.map((ritem, rindex) => {
+                if (ritem.pai_vendor_ticket_number !== 'null') {
+                    // console.log('JIRA filho_ticket_number: ', (ritem.pai_vendor_ticket_number !== 'null' ? ritem.pai_vendor_ticket_number : ritem.filho_ticket_number), ' - filho_submit_date: ', ritem.filho_submit_date, ' - filho_updated_date: ', ritem.filho_updated_date, ' - filho_closed_date:', ritem.filho_closed_date)
+                    line += (ritem.pai_vendor_ticket_number !== 'null' ? ritem.pai_vendor_ticket_number : ritem.filho_ticket_number) + ';' + (ritem.filho_submit_date ? ritem.filho_submit_date : 'N/A') + ';' + (ritem.filho_updated_date ? ritem.filho_updated_date : 'N/A') + ';' + (ritem.filho_closed_date ? ritem.filho_closed_date : 'N/A') + ';'
+                } else {
+                    // console.log('REMEDY filho_ticket_number: ', (ritem.pai_vendor_ticket_number !== 'null' ? ritem.pai_vendor_ticket_number : ritem.filho_ticket_number), ' - filho_submit_date: ', ritem.filho_submit_date, ' - filho_updated_date: ', ritem.filho_updated_date, ' - filho_closed_date:',  ritem.filho_closed_date)  
+                }
+            })
+            line += '\n'
+            // console.log('\n')
+
+
+        })
+        console.log(count)
+        console.log(line.replace(/null/g, '').replace(/N\/A/g, ''))
+
+    }
+    
 
     getCurrentSession() {
         // console.log('getCurrentSession')
@@ -43,8 +93,8 @@ export class HomeContainer extends Container {
                 data.map((item, index) => {
 
                     // Create a big group
-                    if(item && item.pai_product_model_version){
-                        if(item.pai_product_model_version.split('/').length >= 1) {
+                    if (item && item.pai_product_model_version) {
+                        if (item.pai_product_model_version.split('/').length >= 1) {
                             item.pai_biggroup = item.pai_product_model_version.split('/')[0]
                         } else {
                             console.log('Creating a big group: false')
@@ -55,8 +105,8 @@ export class HomeContainer extends Container {
                     if (item.relations.length) {
 
                         item.relations.map((citem, cindex) => {
-                            
-                            if(citem.filho_status.trim() !== 'Done' && citem.filho_status.trim() != 'Closed' && citem.filho_status.trim() != 'Resolved' && citem.filho_status.trim() != null){
+
+                            if (citem.filho_status.trim() !== 'Done' && citem.filho_status.trim() != 'Closed' && citem.filho_status.trim() != 'Resolved' && citem.filho_status.trim() != null) {
                                 item.pai_status = 'Open'
                                 item.pai_closed_date = 'null'
                             }
@@ -71,13 +121,13 @@ export class HomeContainer extends Container {
             this.setState({
                 ticketList: data
             },
-            () => {
+                () => {
 
-                // console.log(
-                //     this.state.ticketList
-                // )
+                    // console.log(
+                    //     this.state.ticketList
+                    // )
 
-            })
+                })
 
             this.setState({
                 isLoadingSession: false
